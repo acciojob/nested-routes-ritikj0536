086b679d90b9bb84a8769ef3513bfc457febde0a
ritikj0536 <jharitik64@gmail.com>
2025-12-17 17:28:11 +0530

    nested-router5

--- src/components/Women.js
+++ src/components/Women.js
@@ -1,57 +1,43 @@
-import React, { useState } from "react";
+import React from "react";
 import { Route, Switch, Link, useRouteMatch } from "react-router-dom";
-import Shirt from "./Shirt";
 import Grooming from "./Grooming";
+import Shirt from "./Shirt";
 import Trouser from "./Trouser";
 import Jewellery from "./Jewellery";
 
 const Women = () => {
   const { path, url } = useRouteMatch();
-  const [history, setHistory] = useState([]);
-
-  const addHistory = (item) => {
-    setHistory([...history, item]);
-  };
 
   return (
     <div>
       <h2>Women Category</h2>
 
-      <ul>
-        <li>
-          <Link to={`${url}/Shirt`} onClick={() => addHistory("Shirt")}>
-            Shirt
-          </Link>
-        </li>
-        <li>
-          <Link to={`${url}/Grooming`} onClick={() => addHistory("Grooming")}>
-            Grooming
-          </Link>
-        </li>
-        <li>
-          <Link to={`${url}/Trouser`} onClick={() => addHistory("Trouser")}>
-            Trouser
-          </Link>
-        </li>
-        <li>
-          <Link to={`${url}/Jewellery`} onClick={() => addHistory("Jewellery")}>
-            Jewellery
-          </Link>
-        </li>
-      </ul>
-
-      {/* HISTORY LIST (VERY IMPORTANT FOR CYPRESS) */}
-      <ul>
-        {history.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <nav>
+        <ul>
+          <li>
+            <Link to={`${url}/grooming`}>Grooming</Link>
+          </li>
+          <li>
+            <Link to={`${url}/shirt`}>Shirt</Link>
+          </li>
+          <li>
+            <Link to={`${url}/trouser`}>Trouser</Link>
+          </li>
+          <li>
+            <Link to={`${url}/jewellery`}>Jewellery</Link>
+          </li>
+        </ul>
+      </nav>
 
       <Switch>
-        <Route path={`${path}/Shirt`} component={Shirt} />
-        <Route path={`${path}/Grooming`} component={Grooming} />
-        <Route path={`${path}/Trouser`} component={Trouser} />
-        <Route path={`${path}/Jewellery`} component={Jewellery} />
+        <Route exact path={path}>
+          <p>Please select a category</p>
+        </Route>
+
+        <Route path={`${path}/grooming`} component={Grooming} />
+        <Route path={`${path}/shirt`} component={Shirt} />
+        <Route path={`${path}/trouser`} component={Trouser} />
+        <Route path={`${path}/jewellery`} component={Jewellery} />
       </Switch>
     </div>
   );
